49401bd72450d312014a2260c90ceaa6656febb1
Joel N. Evans <evansjoe002@icloud.com>
2021-02-20 16:20:03 -0500

    I am Poor

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# I am Rich
- Rich App
+# I am Poor
+ A modification of the "I am Rich" app found at https://github.com/Dav1212/I-am-Rich.
--- i_am_poor/lib/main.dart
+++ i_am_poor/lib/main.dart
@@ -3,18 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: Colors.blue,
       appBar: AppBar(
-        title: Text("I Am Rich"),
-        backgroundColor: Colors.blueGrey[900],
+        title: Text("I Am Poor"),
+        backgroundColor: Colors.blueAccent[700],
       ),
       body: Center(
         child: Image(
-          image: AssetImage('images/diamond.png'),
+          image: AssetImage('images/penny.png'),
         ),
       ),
     ),
   ));
 }
-
-
